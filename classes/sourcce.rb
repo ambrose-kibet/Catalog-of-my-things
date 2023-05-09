e40feb2d7e4e8fce50e066e8ded19999439102f9
ambrose-kibet <ambrosekibet36@gmail.com>
2023-05-09 11:30:14 +0300

    create source class

---
+++ classes/sourcce.rb
@@ -0,0 +1,14 @@
+class Source
+attr_reader :id, :items
+attr_accessor :name,
+
+def initialize(name)
+@id=Random.rand(1..1000)
+@name=name
+@items=[]
+end
+
+def add_item(item)
+@items<<item unless @items.include? item
+end
+end
