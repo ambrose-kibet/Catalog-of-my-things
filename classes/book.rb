b23d893991c12b0f6b3b9aec8eb265574c5ee792
Yemisirach <yemsrach3723@gmail.com>
2023-05-10 12:05:37 +0300

    fix linter error

--- classes/book.rb
+++ classes/book.rb
@@ -1,9 +1,8 @@
 class Book
   attr_accessor :cover_state, :publisher, :published_date
 
-  def initialize(id, genre, author, source, label, cover_state, publisher,
-                 published_date)
-    super(id, genre, author, source, label, published_date)
+  def initialize(publisher)
+    super(id, genre, author, label, published_date)
     @cover_state = cover_state
     @publisher = publisher
   end
