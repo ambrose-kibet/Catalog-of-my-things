83179e54d19eddebc50d9a7b82d9650da2ad1f63
jonas-45 <jonaswullah@gmail.com>
2023-05-10 12:48:00 +0000

    Add item class with constructor

---
+++ item.rb
@@ -0,0 +1,11 @@
+class item
+  def initialize(id, genre, author, source, label, published_date, archived)
+    @id = id
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @published_date = published_date
+    @archived = archived
+  end
+end
