8b15f31dc2690418772802282f29f49557d1c9a0
Yemisirach <yemsrach3723@gmail.com>
2023-05-10 11:55:29 +0300

    fix linter error

--- classes/game.rb
+++
@@ -1,9 +0,0 @@
-class Game
-  attr_reader :multiplayer, :last_played_at
-
-  def initialize(multiplayer, last_played_at, id, genre, author, source, label, published_date)
-    @multiplayer = multiplayer
-    @last_played_at = last_played_at
-    super(id, genre, author, source, label, published_date)
-  end
-end
