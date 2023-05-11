64f66bd7bcd4a872613fa14b75dd6738d398d1cc
Yemisirach <yemsrach3723@gmail.com>
2023-05-11 10:03:11 +0300

    fix linter error

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -9,7 +9,6 @@ describe Book do
     expect(@book).to be_an_instance_of(Book)
   end
 
-
   it 'should be able to be archived' do
     can_be_archived = @book.send(:can_be_archived?)
     expect(can_be_archived).to eq(true)
