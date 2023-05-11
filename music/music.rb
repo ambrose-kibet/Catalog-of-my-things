8306ce8aa3c45dfb40aa7a05d0c331e9b75c4380
jonas-45 <jonaswullah@gmail.com>
2023-05-11 05:50:51 +0000

    Refactor music album constructor by removing super method

--- music/music.rb
+++ music/music.rb
@@ -1,14 +1,15 @@
-class MusicAlbum
+require './item'
+
+class MusicAlbum < Item
   attr_reader :on_spotify
 
-  def initialize(on_spotify, *args)
-    super(*args)
+  def initialize(on_spotify)
     @on_spotify = on_spotify
   end
 
   private
 
-  def can_be_achived?
+  def can_be_archived?
     return true if super && on_spotify
   end
 end
